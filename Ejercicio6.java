a42b6dcf3a8493a397c4d9f265fd9a9ec81ffa13
Yefri Gonzalez <yefrig00@gmail.com>
2021-06-09 01:33:00 -0600

    Agregue el Ejercicio6.java

---
+++ Ejercicio6.java
@@ -0,0 +1,13 @@
+
+public class Ejercicio6{
+	public static void main(String[] args){
+		double numero=(Math.random()*200);
+		double aumentado30;
+		System.out.println("El numero aleatorio es: "+numero);
+		aumentado30=(numero+(numero*0.30));
+		System.out.println("El numero aumentado en 30% es: "+aumentado30);
+	}
+}
+
+
+
