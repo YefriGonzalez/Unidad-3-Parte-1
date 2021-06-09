5f77d5a2552882f65bfdb2a58cab0a6aca63dfd4
Yefri Gonzalez <yefrig00@gmail.com>
2021-06-09 01:33:20 -0600

    Agregue el Ejercicio7.java

---
+++ Ejercicio7.java
@@ -0,0 +1,11 @@
+public class Ejercicio7{
+	public static void main(String[] args){
+		double numero=(Math.random()*(50-10))+10;
+		double disminuido15;
+		System.out.println("El numero aleatorio es: "+numero);
+		disminuido15=numero-(numero*0.15);
+		System.out.println("El numero disminuido en 15% es: "+disminuido15);
+	
+	}
+
+}
