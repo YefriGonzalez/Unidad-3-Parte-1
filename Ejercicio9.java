ae765be0f6be6743f667b9c48984e490ab41d17b
Yefri Gonzalez <yefrig00@gmail.com>
2021-06-09 01:34:27 -0600

    Agregue el Ejercicio9.java

---
+++ Ejercicio9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ejercicio9{
+	public static void main(String[] args){
+		Scanner scanner =new Scanner(System.in);
+		String A;
+		String B;
+		System.out.println("Ingrese la primera palabra");
+		A=scanner.nextLine();
+		System.out.println("Ingrese la primera palabra");
+		B=scanner.nextLine();
+		System.out.println("A: "+B);
+		System.out.println("B: "+A);
+	}
+}
