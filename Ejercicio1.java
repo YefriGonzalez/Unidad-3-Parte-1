9b97e6f1816af3c5c52d677bc4d8d7d4f5760dec
Yefri Gonzalez <yefrig00@gmail.com>
2021-06-09 01:27:32 -0600

    Agregue el Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ejercicio1{
+	public static void main(String[] args){
+		Scanner scanner=new Scanner(System.in);
+		int numero1;
+		int numero2;
+		int suma;
+		
+		System.out.print("Escriba el primer numero: ");
+		numero1=scanner.nextInt();
+		System.out.print("Escriba el segundo numero: ");
+		numero2=scanner.nextInt();
+		suma= numero1+numero2;
+		System.out.println("La suma es: "+suma);
+	}
+
+
+
+
+
+}
